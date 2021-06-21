25c12dbdbd57695f8410b233c4bb1296ca44a19f
Ayush Jain <ayushjain051001@gmail.com>
2021-06-21 23:50:29 +0530

    Update project.wsgi.py

--- project.wsgi.py
+++ project.wsgi.py
@@ -5,5 +5,4 @@ project_home = ''
 if project_home not in sys.path:
     sys.path = [project_home] + sys.path
 
-# import flask app but need to call it "application" for WSGI to work
-f
+# import flask app but need to call it "application" for WSGI to
