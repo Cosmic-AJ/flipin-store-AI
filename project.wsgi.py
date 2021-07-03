3d81688374eb5f7506500daf320c7a7a07fb9709
Ayush Jain <ayushjain051001@gmail.com>
2021-07-03 19:06:37 +0530

    Update project.wsgi.py

--- project.wsgi.py
+++ project.wsgi.py
@@ -3,4 +3,4 @@ import sys
 # add your project directory to the sys.path
 project_home = ''
 if project_home not in sys.path:
-    sys.path = [project_home] + sys.
+    sys.path = [project_home] + sy
